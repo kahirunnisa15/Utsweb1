383b9fce268ffc6f31ec1eed61a796858dd2dbfc
kahirunnisa15 <kahirunnisa25@gmail.com>
2025-11-07 09:56:49 +0700

    logout

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-session_destroy();
-header("Location: login.php");
+session_unset(); // Hapus semua data session
+session_destroy(); // Hapus session dari server
+
+header("Location: index.php");
 exit;
 ?>
